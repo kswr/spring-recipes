d64e4b04b3a4cd1317e8281b393c499fd12667cd
kswr <ksawery.oleszynski@gmail.com>
2019-02-24 00:26:22 +0100

    added tests utilizing MockMVC feature

--- src/main/java/com/kswr/spring/recipes/springrecipes/controllers/IndexController.java
+++ src/main/java/com/kswr/spring/recipes/springrecipes/controllers/IndexController.java
@@ -1,13 +1,10 @@
 package com.kswr.spring.recipes.springrecipes.controllers;
 
-import com.kswr.spring.recipes.springrecipes.domain.Recipe;
 import com.kswr.spring.recipes.springrecipes.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Set;
 
 @Slf4j
 @Controller
@@ -21,9 +18,8 @@ public class IndexController {
 
     @RequestMapping({"","/","/index","/index.html"})
     public String getIndexPage(Model model) {
-        Set<Recipe> recipes = recipeService.getRecipes();
-        model.addAttribute("recipes", recipes);
         log.debug("Getting index page");
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 }
