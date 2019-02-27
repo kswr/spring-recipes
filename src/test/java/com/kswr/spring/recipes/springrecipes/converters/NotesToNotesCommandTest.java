ed8a65f9cb122ee8cefc60a5da185f0dfc807997
kswr <ksawery.oleszynski@gmail.com>
2019-02-27 23:49:09 +0100

    added test for one converter, added stubs for the rest

---
+++ src/test/java/com/kswr/spring/recipes/springrecipes/converters/NotesToNotesCommandTest.java
@@ -0,0 +1,18 @@
+package com.kswr.spring.recipes.springrecipes.converters;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+// TODO implement test
+public class NotesToNotesCommandTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void convert() {
+    }
+}
