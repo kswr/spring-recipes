9f71d2b0739eafdef17f5cdb02d047fd3c51d3e9
kswr <ksawery.oleszynski@gmail.com>
2019-02-27 16:26:23 +0100

    added command pojos

---
+++ src/main/java/com/kswr/spring/recipes/springrecipes/commands/NotesCommand.java
@@ -0,0 +1,14 @@
+package com.kswr.spring.recipes.springrecipes.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class NotesCommand {
+    private Long id;
+    private RecipeCommand recipe;
+    private String recipeNotes;
+}
